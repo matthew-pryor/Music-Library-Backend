f51de917895c7932edc3fba324902e7a92293a6a
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-10 11:37:45 -0600

    created music_library_database + created models

--- music_library/models.py
+++ music_library/models.py
@@ -1,3 +1,13 @@
+from turtle import title
+from django.db import models
+from django.forms import CharField, DateField
 from django.db import models
 
 # Create your models here.
+
+class Music(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=250)

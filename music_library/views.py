5ab2ac156d73cf1e09b36c6c6cd73bd805dc718b
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-10 13:46:28 -0600

    created GET and POST requests for all music within the server/data set

--- music_library/views.py
+++ music_library/views.py
@@ -8,9 +8,21 @@ from .models import Music
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_library_list(request):
-    pass
+    
+    if request.method == 'GET':
+
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def music_library_detail(request, pk):
